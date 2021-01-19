1fcc692e79b7425043943b45a3856bdabce8c962
Michelle Rossouw <64898873+MichelleRos@users.noreply.github.com>
2021-01-19 14:56:46 +1100

    renamed file

---
+++ ArduBlimp/Fins.h
@@ -0,0 +1,10 @@
+//This class is for converting horizontal acceleration commands to fin flapping commands.
+class Fins {      //mixer.h
+public:
+    friend class Blimp;
+    Fins(void);
+private:
+    
+
+
+};
