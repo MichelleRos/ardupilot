656135d48dd0125d7f5a1ef8757c531eab8df19e
Michelle Rossouw <64898873+MichelleRos@users.noreply.github.com>
2021-03-10 10:30:44 +1100

    Blimp: Fix yaw numbers for rudder arming.

--- ArduBlimp/motors.cpp
+++ ArduBlimp/motors.cpp
@@ -26,7 +26,7 @@ void Blimp::arm_motors_check()
     int16_t yaw_in = channel_yaw->get_control_in();
 
     // full right
-    if (yaw_in > 4000) {
+    if (yaw_in > 900) {
 
         // increase the arming counter to a maximum of 1 beyond the auto trim counter
         if (arming_counter <= AUTO_TRIM_DELAY) {
@@ -49,7 +49,7 @@ void Blimp::arm_motors_check()
         }
 
     // full left and rudder disarming is enabled
-    } else if ((yaw_in < -4000) && (arming_rudder == AP_Arming::RudderArming::ARMDISARM)) {
+    } else if ((yaw_in < -900) && (arming_rudder == AP_Arming::RudderArming::ARMDISARM)) {
         if (!flightmode->has_manual_throttle() && !ap.land_complete) {
             arming_counter = 0;
             return;
