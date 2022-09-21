2ef821fa53338dd53cec6f01eec4054bf9d3bcd3
Michelle Rossouw <michelleros128@gmail.com>
2022-09-21 15:30:25 +1000

    SITL: Update multi-mavproxy to slow down stream rate etc

--- libraries/SITL/examples/multi-mavproxy.sh
+++ libraries/SITL/examples/multi-mavproxy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-mavproxy.py --continue --master=mcast:
-# --cmd='module load srcloc; map set showgpspos 0; map set showgps2pos 0; slp 6; module load swarm; module unload mir; sl sethome -35.2802552 149.0058478'
+mavproxy.py --console --map --master=mcast: --cmd='module load srcloc; set streamrate 1; set streamrate2 1; layout load; map set showgpspos 0; map set showgps2pos 0; slp 6; module load swarm; sl sethome -35.2802552 149.0058478'
